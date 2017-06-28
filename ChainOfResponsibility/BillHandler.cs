2c1d9f92c0649459cf8c579f934eec2d8cf94737
韩彭飞 <307788836@qq.com>
2017-06-28 09:11:10 +0800

    责任链

--- ChainOfResponsibility/BillHandler.cs
+++ ChainOfResponsibility/BillHandler.cs
@@ -67,30 +67,95 @@ namespace ChainOfResponsibility
                 bill.Status = BillStatus.Submitted;
                 Console.WriteLine(string.Format("{0}：{1}已经提交！", this.UserName, bill.BilNo));
             }
+            if(CheckPermission("AUDIT")&&bill.Status==BillStatus.Submitted)
+            {
+                if(bill.Amount<=5000)
+                {
+                    bill.Status = BillStatus.Submitted;
+                    Console.WriteLine(string.Format("{0}：{1}已经审核！", this.UserName, bill.BilNo));
+                }
+                else
+                {
+                    this.Successor.DoBillOperation(bill);
+                }
+            }
+            else
+            {
+                this.Successor.DoBillOperation(bill);
+            }
         }
     }
 
     public class Manager : BillHandler
     {
+        private List<string> permissions = new List<string>() { "SAVE", "SUBMIT", "AUDIT" };
         public Manager(string userName)
         {
-
+            base.UserName = userName;
+            base.Permissions = permissions;
+            base.Successor = new CEO("CEO--链总");
         }
         public override void DoBillOperation(Bill bill)
         {
-            throw new NotImplementedException();
+            if (CheckPermission("SAVE") && bill.Status == BillStatus.Open)
+            {
+                bill.Status = BillStatus.Saved;
+                Console.WriteLine(string.Format("{0}：{1}已经保存！", this.UserName, bill.BilNo));
+            }
+
+            if (CheckPermission("SUBMIT") && bill.Status == BillStatus.Saved)
+            {
+                bill.Status = BillStatus.Submitted;
+                Console.WriteLine(string.Format("{0}：{1}已经提交！", this.UserName, bill.BilNo));
+            }
+
+            if (CheckPermission("AUDIT") && bill.Status == BillStatus.Submitted)
+            {
+                if (bill.Amount <= 20000)
+                {
+                    bill.Status = BillStatus.Submitted;
+                    Console.WriteLine(string.Format("{0}：{1}已经审核！", this.UserName, bill.BilNo));
+                }
+                else
+                {
+                    this.Successor.DoBillOperation(bill);
+                }
+
+            }
+            else
+            {
+                this.Successor.DoBillOperation(bill);
+            }
         }
     }
 
     public class CEO : BillHandler
     {
+        private List<string> permissions = new List<string>() { "SAVE", "SUBMIT", "AUDIT" };
         public CEO(string userName)
         {
-
+            base.UserName = userName;
+            base.Permissions = permissions;
         }
         public override void DoBillOperation(Bill bill)
         {
-            throw new NotImplementedException();
+            if (CheckPermission("SAVE") && bill.Status == BillStatus.Open)
+            {
+                bill.Status = BillStatus.Saved;
+                Console.WriteLine(string.Format("{0}：{1}已经保存！", this.UserName, bill.BilNo));
+            }
+
+            if (CheckPermission("SUBMIT") && bill.Status == BillStatus.Saved)
+            {
+                bill.Status = BillStatus.Submitted;
+                Console.WriteLine(string.Format("{0}：{1}已经提交！", this.UserName, bill.BilNo));
+            }
+
+            if (CheckPermission("AUDIT") && bill.Status == BillStatus.Submitted)
+            {
+                bill.Status = BillStatus.Submitted;
+                Console.WriteLine(string.Format("{0}：{1}已经审核！", this.UserName, bill.BilNo));
+            }
         }
     }
 }
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChainOfResponsibility
 {
@@ -10,6 +6,21 @@ namespace ChainOfResponsibility
     {
         static void Main(string[] args)
         {
+            PurchaseBill bill = new PurchaseBill()
+            {
+                BilNo = "CGDD001",
+                MaterialName = "惠普电脑",
+                Qty = 50,
+                Price = 5000,
+                //BillMaker = new Purchaser("采购员--小责")
+                BillMaker = new Manager("经理--任经理")
+                //BillMaker = new CEO("CEO--链总")
+            };
+            Console.WriteLine(string.Format("创建采购申请单：{0};申请购买{1}台{2}", bill.BilNo, bill.Qty, bill.MaterialName));
+
+            bill.BillMaker.HandleBill(bill);
+
+            Console.ReadLine();
         }
     }
 }
